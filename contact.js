5da8ae5b994673cc1dbfaee1bcbf6302f6f3d3ef
Vipulmahajan045 <vipulmahajan170140111045@gmail.com>
2021-07-16 22:47:38 +0530

    done

---
+++ contact.js
@@ -0,0 +1,19 @@
+//here we used jquery
+$(document).ready(function(){
+    function init(){
+        if (localStorage["name"]){
+            $('#name').val(localStorage["name"]);
+
+        }
+        if(localStorage["email"]){
+            $('#email').val(localStorage["email"]);
+        }
+        if(localStorage["message"]){
+            $('#message').val(localStorage["message"]);
+        }
+    }
+    init();
+});
+$('.stored').change(function(){
+    localStorage[$(this).attr('name')] = $(this).val();
+});
